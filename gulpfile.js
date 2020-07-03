6dbd3d2bef8217abac387035d2709e73f95bdbbd
savageblossom <umer.muradasiloff@gmail.com>
2020-07-03 14:10:17 +0300

    reorganized project

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      browserSync = require('browser-sync').create();
+
+const paths = {
+    styles: {
+        src: 'app/scss/style.scss',
+        dest: 'app'
+    }
+}
+
+const style = () => {
+    return (
+        gulp
+            .src(paths.styles.src)
+            .pipe(sass())
+            .on('error', sass.logError)
+            .pipe(gulp.dest(paths.styles.dest))
+            .pipe(browserSync.stream())
+    )
+}
+
+const reload = () => {
+    browserSync.reload();
+}
+
+const watch = () => {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    })
+    gulp.watch('app/scss/**.scss', style)
+    gulp.watch(['app/**.html', 'scss/**.scss'], reload)
+}
+
+exports.style = style;
+exports.watch = watch;
